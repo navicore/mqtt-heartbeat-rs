c127fdf12063f95a33ba4d8c435342c92e03a559
Ed Sweeney <ed@onextent.com>
2018-08-05 09:37:56 -0700

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate actix;
 extern crate config;
+extern crate futures;
 extern crate mqttc;
 extern crate netopt;
 extern crate serde;
 extern crate ws;
-
-//use mqttc::{PubOpt, PubSub};
-use settings::Settings;
-//use std::str;
-//use std::sync::Arc;
-//use std::time;
+use actix::*;
+use futures::{future, Future};
+use heartbeater::{Beat, Heartbeater};
+mod heartbeater;
 mod settings;
 
 fn main() {
-    let settings = Settings::new().unwrap();
+    let system = actix::System::new("test");
 
-    let json = format!(
-        r#"{{"heartbeat": "{} {}"}}"#,
-        settings.heartbeat_template, "me"
-    );
-    println!("{}", json);
+    let addr = Heartbeater.start();
+    let res = addr.send(Beat());
+    Arbiter::spawn(res.then(|res| {
+        match res {
+            Ok(result) => println!("Beat: {}", result),
+            Err(err) => println!("Bad beat: {}", err),
+        }
+
+        System::current().stop();
+        future::result(Ok(()))
+    }));
+    system.run();
 }
